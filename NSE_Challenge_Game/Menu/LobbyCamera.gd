591d71bb4f52ce502f01409d68ddd8cfa1d48579
Arr0gant <albertoarjanlee@gmail.com>
2021-02-26 13:01:14 +0000

    Transition Camera pitch shift

--- NSE_Challenge_Game/Menu/LobbyCamera.gd
+++ NSE_Challenge_Game/Menu/LobbyCamera.gd
@@ -8,6 +8,11 @@ onready var pos_vec = (tp.transform.origin-transform.origin)
 onready var rot_vec_x = (tp.transform.basis.x-transform.basis.x)
 onready var rot_vec_y = (tp.transform.basis.y-transform.basis.y)
 onready var rot_vec_z = (tp.transform.basis.z-transform.basis.z)
+onready var ambiance = get_parent().get_node("Ambiance")
+
+func _ready():
+	ambiance.pitch_scale = 0.1
+	
 
 func _process(delta):
 	if(travelling):
@@ -16,13 +21,14 @@ func _process(delta):
 func travel_to_locrot(spd):
 	if((transform.origin-tp.transform.origin).length() < 1):
 		travelling = false
-	
 	if((transform.origin-tp.transform.origin).length() < 20):
 		set_zfar(1)
 
 	var d = transform.origin-tp.transform.origin
 	if(speed < 0.4):
 		speed += 0.0002
+	if(ambiance.pitch_scale < 0.5):
+		ambiance.pitch_scale += 0.0003
 	transform.origin += pos_vec*spd
 	transform.basis.y += rot_vec_y*spd
 	transform.basis.z += rot_vec_z*spd
