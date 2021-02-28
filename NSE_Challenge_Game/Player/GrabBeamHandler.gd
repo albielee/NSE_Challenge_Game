143475b14d9adad23e7211e14f585db166648338
rrrama <danielplcrooks@gmail.com>
2021-02-28 13:21:56 +0000

    grabbeam maybe

--- NSE_Challenge_Game/Player/GrabBeamHandler.gd
+++ NSE_Challenge_Game/Player/GrabBeamHandler.gd
@@ -7,12 +7,31 @@ onready var grabbed_rock = get_tree().get_root().get_node("/root/World/TEMP")
 
 var old_rot = 0
 var scalar = 10
+var active = false
+
+func start_beam(rock):
+	grabbed_rock = rock
+	update_position()
+	active = true
+	visible = true
+
+func stop_beam():
+	active = false
+	visible = false
+
+
 
 func _physics_process(delta):
+	if active:
+		update_position()
+		
+
+
+func update_position():
 	var rock_pos = grabbed_rock.get_global_transform().origin
 	var pos = get_global_transform().origin
 	var dist_to_rock = sqrt(pow((rock_pos.x-pos.x),2)+pow((rock_pos.z-pos.z),2))
-	
+
 	var dif = get_global_transform().basis.get_euler().y-old_rot
 	old_rot = get_global_transform().basis.get_euler().y
 	if dif>PI/2:
@@ -24,10 +43,9 @@ func _physics_process(delta):
 	var spacing = dist_to_rock/particles_count
 	var sine_input = PI/particles_count
 	
-	#1 to particles count because we dont want the spacing of the first particle to be on the space e.g. 0
+#1 to particles count because we dont want the spacing of the first particle to be on the space e.g. 0
 	for i in range(1, particles_count+1):
 		particles[i-1].global_transform.origin = pos + Vector3(
 			spacing*i*cos(angle) + sin(sine_input*i)*dif*scalar*spacing*sin(angle), 
 			newy.y*i, 
 			spacing*i*sin(angle) - sin(sine_input*i)*dif*scalar*spacing*cos(angle))
-		
